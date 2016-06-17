c11f758ef61c41cf600a61120a11a447044e9f45
Jake Craige <james.craige@gmail.com>
2016-06-17 15:14:14 -0700

    Initial project generation

---
+++ FirebaseInFrameworkExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseInFrameworkExample
+//
+//  Created by James Craige on 6/17/16.
+//  Copyright © 2016 thoughtbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
